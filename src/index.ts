6fd2f3f760e3239b78d7aeaf0d953a1c5389aea1
rferg <rmferguson77@gmail.com>
2020-05-19 16:30:40 -0400

    remove npmignore, add reflect-metadata check

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+import 'reflect-metadata'
+
+if (!Reflect || !Reflect.getMetadata) {
+  throw new Error(`No reflection polyfill found.  Please add "import 'reflect-metadata'".`)
+}
+
 export { ElementRegistration, ElementRegistrar as ElementRegistry } from '../src/elements'
 export {
   ClassTypeProvider,
